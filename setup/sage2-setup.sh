5c6724447d5ab3e3b5aaaa475dd72b6750833c35
Owen Kaluza <owen.kaluza@monash.edu>
2018-10-05 15:51:30 +1000

    Add config file to sage2 setup

--- setup/sage2-setup.sh
+++ setup/sage2-setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 #https://bitbucket.org/sage2/sage2/wiki/Home
 #https://bitbucket.org/sage2/sage2/wiki/Install%20(Docker)
+if [ $# -eq 0 ]
+then
+  echo "No arguments supplied, please pass json config file to use"
+fi
 
 docker pull sage2/master
 docker create -v /sage2/config --name sage2Config sage2/master
@@ -8,6 +12,9 @@ docker create -v /sage2/keys --name sage2Keys sage2/master
 docker create -v /root/Documents/SAGE2_Media --name sage2Uploads sage2/master
 docker run --rm -it --volumes-from sage2Keys sage2/master /sage2/keys/GO-docker _.cave.monash.edu
 docker run --rm -it --volumes-from sage2Config sage2/master /bin/bash
+
+#copy config file into /sage2/config/docker-cfg.json
+docker cp $1 sage2/master:/sage2/config/docker-cfg.json
 
 #yum install ./google-chrome-stable_current_x86_64.rpm
 #wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
